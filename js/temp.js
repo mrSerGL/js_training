deba9a51874fdf7febf47a757f7590ee1f97b0c3
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2022-12-30 15:02:39 +0200

    current 221230-1503

--- js/temp.js
+++ js/temp.js
@@ -1,25 +1,27 @@
-function multiply(x, y, z) {
-    console.log("Код до return выполняется как обычно");
+// function multiply(x, y, z) {
+//     console.log("Код до return выполняется как обычно");
   
-    // Возвращаем результат выражения умножения
-    return x * y * z;
+//     // Возвращаем результат выражения умножения
+//     return x * y * z;
   
-    console.log("Этот лог никогда не выполнится, он стоит после return");
-  }
+//     console.log("Этот лог никогда не выполнится, он стоит после return");
+//   }
 
-// Результат работы функции можно сохранить в переменную
-let result = multiply(2, 3, 5);
-console.log(result); 
+// // Результат работы функции можно сохранить в переменную
+// let result = multiply(2, 3, 5);
+// console.log(result); 
 
-result = multiply(4, 8, 12);
-console.log(result); // 384
+// result = multiply(4, 8, 12);
+// console.log(result); // 384
 
-result = multiply(17, 6, 25);
-console.log(result); // 2550
+// result = multiply(17, 6, 25);
+// console.log(result); // 2550
 
-console.log('==========================');
+// console.log('==========================');
 // const valueB = "random string";
 // console.log(Number(valueB));
 // console.log(typeof Number(valueB));
+
+
 
 
